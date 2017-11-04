dd993f852d255affeadbcec5c51675e198ef0264
Ye Min Naing <thargyi74@gmail.com>
2017-11-04 17:48:53 +0630

    Initialize skeleton project with default structure / value.

---
+++ app/src/main/java/com/yeminnaing/sfc/SFCNewsApp.java
@@ -0,0 +1,17 @@
+package com.yeminnaing.sfc;
+
+import android.app.Application;
+
+/**
+ * Created by yeminnaing on 11/4/17.
+ */
+
+public class SFCNewsApp extends Application {
+
+    public static final String LOG_TAG = "SFCNewsApp";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
